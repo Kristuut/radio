7a312b835ddf030593b78813cbb4904088164881
Kristina Sachenko <mytino4ka@mail.ru>
2020-10-29 01:09:47 +0700

    New branch "Lombok". Modified pom.

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -1,5 +1,13 @@
 package ru.netology;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
 public class Radio {
     private int currentRadioStation;
     private int firstRadioStation = 0;
