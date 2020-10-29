4a4d747728c58666508a52db0831fbb64c75ac31
Kristina Sachenko <mytino4ka@mail.ru>
2020-10-30 00:35:57 +0700

    Added constructor

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -4,55 +4,30 @@ public class Radio {
     private int currentRadioStation;
     private int firstRadioStation = 0;
     private int lastRadioStation = 10;
+    private int amountRadioStation = 10;
     private int currentSoundVolume;
     private int minSoundVolume = 0;
     private int maxSoundVolume = 100;
 
-    public int getCurrentRadioStation() {
-        return currentRadioStation;
-    }
-
-    public void setCurrentRadioStation(int currentRadioStation) {
+    public Radio(int currentRadioStation,
+                 int firstRadioStation,
+                 int lastRadioStation,
+                 int amountRadioStation) {
         if (currentRadioStation > lastRadioStation)
             return;
         if (currentRadioStation < firstRadioStation)
             return;
         this.currentRadioStation = currentRadioStation;
-    }
 
-    public int getFirstRadioStation() {
-        return firstRadioStation;
-    }
-
-    public int getLastRadioStation() {
-        return lastRadioStation;
-    }
-
-    public int getCurrentSoundVolume() {
-        return currentSoundVolume;
-    }
-
-    public void setCurrentSoundVolume(int currentSoundVolume) {
-        if (currentSoundVolume > maxSoundVolume)
-            return;
-        if (currentSoundVolume < minSoundVolume)
-            return;
-        this.currentSoundVolume = currentSoundVolume;
-    }
-
-    public int getMinSoundVolume() {
-        return minSoundVolume;
-    }
-
-    public int getMaxSoundVolume() {
-        return maxSoundVolume;
+        this.firstRadioStation = firstRadioStation;
+        this.lastRadioStation = lastRadioStation;
+        this.amountRadioStation = amountRadioStation;
     }
 
     public void changeNextRadioStation() {
         if (currentRadioStation == lastRadioStation) {
             this.currentRadioStation = firstRadioStation;
-        }
-        else {
+        } else {
             currentRadioStation++;
         }
     }
@@ -65,6 +40,32 @@ public class Radio {
         }
     }
 
+    public int getCurrentRadioStation() {
+        return currentRadioStation;
+    }
+
+    public int getFirstRadioStation() {
+        return firstRadioStation;
+    }
+
+    public int getLastRadioStation() {
+        return lastRadioStation;
+    }
+
+    public int getAmountRadioStation() {
+        return amountRadioStation;
+    }
+
+    public Radio(int currentSoundVolume, int minSoundVolume, int maxSoundVolume) {
+        if (currentSoundVolume > maxSoundVolume)
+            return;
+        if (currentSoundVolume < minSoundVolume)
+            return;
+        this.currentSoundVolume = currentSoundVolume;
+        this.minSoundVolume = minSoundVolume;
+        this.maxSoundVolume = maxSoundVolume;
+    }
+
     public void increaseCurrentSoundVolume() {
         if (currentSoundVolume < maxSoundVolume)
             currentSoundVolume++;
@@ -73,5 +74,17 @@ public class Radio {
     public void decreaseCurrentSoundVolume() {
         if (currentSoundVolume > minSoundVolume)
             currentSoundVolume--;
+    }
+
+    public int getCurrentSoundVolume() {
+        return currentSoundVolume;
+    }
+
+    public int getMinSoundVolume() {
+        return minSoundVolume;
+    }
+
+    public int getMaxSoundVolume() {
+        return maxSoundVolume;
     }
 }
